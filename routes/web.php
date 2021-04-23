701c8ae1ff289b7a454b7fcc3e3fc67f220d5945
MohamedAbdelfattah99 <abdelfattahmohamed968@gmail.com>
2021-04-24 00:55:47 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::get('/home',[FactoryController::class, 'index']);
 Route::get('/home/login',[EmployeeController::class,'login']);
 Route::post('/home/login',[EmployeeController::class,'check']);
 Route::middleware([MangerMiddleware::class])->group(function () {
- Route::get('/home/add_employee',[EmployeeController::class,'add_employee']);
-Route::post('/home/add_employee',[EmployeeController::class,'store_employee']);
+ Route::get('/home/add-employee',[EmployeeController::class,'add_employee']);
+Route::post('/home/add-employee',[EmployeeController::class,'store_employee']);
 });
 Route::get('/logout',[EmployeeController::class,'logout']);
 ?>
