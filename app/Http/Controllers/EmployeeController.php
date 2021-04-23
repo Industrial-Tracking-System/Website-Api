843c8608f87799e45b434e3499c497e2ed62357d
MohamedAbdelfattah99 <abdelfattahmohamed968@gmail.com>
2021-04-24 00:55:36 +0200

    Update EmployeeController.php

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 class EmployeeController extends Controller
 {
     /**
