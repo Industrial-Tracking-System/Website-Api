cb8177215537bc9ba878b60117db6814f27a77b3
MohamedAbdelfattah99 <abdelfattahmohamed968@gmail.com>
2021-04-24 16:55:19 +0200

    Create ApiEmployee.php

---
+++ app/Http/Controllers/ApiEmployee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller as Controller;
+
+
+
+class ApiEmployee extends Controller
+{
+    public function index()
+    {
+     return response()->json('asd');   
+    }
+    
+  }
